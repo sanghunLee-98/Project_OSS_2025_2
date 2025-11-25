ada982c9555ed0140dc8a73e5b8a0c86f3ee4cb5
sanghunLee-98 <jo700523@naver.com>
2025-11-26 02:34:55 +0900

    [기능추가] 계산기에 랜덤 숫자 생성 기능 추가
    계산기에 랜덤 숫자 생성 기능 추가하여 0~100까지 랜덤값 생성

--- OSS_Calc/calc.py
+++ OSS_Calc/calc.py
@@ -1,25 +1,23 @@
 import tkinter as tk
-
+import random
 
 class Calculator:
     def __init__(self, root):
         self.root = root
         self.root.title("계산기")
-        self.root.geometry("300x400")
+        self.root.geometry("300x420")
 
         self.expression = ""
 
-        # 입력창
         self.entry = tk.Entry(root, font=("Arial", 24), justify="right")
         self.entry.pack(fill="both", ipadx=8, ipady=15, padx=10, pady=10)
 
-        # 버튼 생성
         buttons = [
             ['7', '8', '9', '/'],
             ['4', '5', '6', '*'],
             ['1', '2', '3', '-'],
             ['0', '.', 'C', '+'],
-            ['=']
+            ['=', 'Rand']   # ← 랜덤 버튼 추가
         ]
 
         for row in buttons:
@@ -37,16 +35,22 @@ class Calculator:
     def on_click(self, char):
         if char == 'C':
             self.expression = ""
+
         elif char == '=':
             try:
                 self.expression = str(eval(self.expression))
-            except Exception:
-                self.expression = "에러"
+            except:
+                self.expression = "error"
+
+        elif char == 'Rand':
+            self.expression = str(random.randint(0, 100))
+
         else:
             self.expression += str(char)
 
         self.entry.delete(0, tk.END)
         self.entry.insert(tk.END, self.expression)
+
 
 
 
